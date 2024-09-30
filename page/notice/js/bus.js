cf9d0e68dc3aad44a3108c9a51800ad5b61a1105
Nguyễn Thanh Như <106093332+navigationcode@users.noreply.github.com>
2024-09-30 15:04:00 +0700

    01/10/2024

--- page/notice/js/bus.js
+++ page/notice/js/bus.js
@@ -1,7 +1,7 @@
 
 
 function playClickBus() {
-const sounds = ["sound/bus.mp3", "sound/0.bus/adele.mp3", "sound/0.bus/ben.mp3", "sound/0.bus/bo.mp3", "sound/0.bus/coca.mp3", "sound/0.bus/dminh.mp3", "sound/0.bus/nicky.mp3"];
+const sounds = ["sound/bus.mp3", "sound/0.bus/adele.mp3", "sound/0.bus/bo.mp3", "sound/0.bus/coca.mp3", "sound/0.bus/dminh.mp3", "sound/0.bus/peter.mp3"];
 const audio = document.getElementById("sound0");
 let delay = 200;
 sounds.forEach(sound => {
